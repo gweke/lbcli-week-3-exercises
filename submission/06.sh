a96e7bd4e55905618fc5967077f0e55491a1589d
William EKE <gwilliam.eke@gmail.com>
2025-04-02 02:29:45 +0100

    adding -r for jq extraction for 06.sh and 07.sh

--- submission/06.sh
+++ submission/06.sh
@@ -4,4 +4,4 @@
 psbt="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
 DECODE_PSBT=$(bitcoin-cli -regtest decodepsbt $psbt)
-echo $DECODE_PSBT | jq '.tx.hash'
+echo $DECODE_PSBT | jq -r '.tx.hash'
